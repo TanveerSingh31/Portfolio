6b9375f37c59b70694f54c1c90b64e1c32b67681
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-04-02 21:26:26 +0530

    about section

---
+++ src/app/components/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+        <div className="flex flex-col gap-6 items-center w-3xl">
+            <h2 className="text-4xl">About</h2>    
+            
+            <img src="https://picsum.photos/200/300" 
+                className="size-32"></img>
+            <p>
+                Fully committed to the philosophy of life-long learning, I’m a full stack developer with a deep passion for JavaScript, React and all things web development. The unique combination of creativity, logic, technology and never running out of new things to discover, drives my excitement and passion for web development. When I’m not at my computer I like to spend my time reading, keeping fit and playing guitar.
+            </p>
+        
+        </div>
+    )
+}
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div className="w-full flex justify-end">
+            <a className="m-2">Home</a>
+            <a className="m-2">About</a>
+            <a className="m-2">Projects</a>
+            <a className="m-2">Contact</a>
+        </div>
+    );
+}
