769ef44d7dcb16bb060aa0aafa8d3e23cc3239f9
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-04-02 23:37:24 +0530

    visual formatting

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -4,9 +4,9 @@ function SkillIcon({name}) {
     console.log(__dirname);
     let skillImagePath = `/${name}.png`;
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center border-1 rounded-2xl my-2 p-2 w-30 h-30'>
             <Image src={skillImagePath} width={50} height={50} />
-            <p className="font-bold mt-1 mb-6">{name}</p>
+            <p className="font-bold mt-1 uppercase">{name}</p>
         </div>
     );
 }
@@ -15,21 +15,22 @@ function SkillIcon({name}) {
 
 export default function Skills() {
     return (
-        <div className="w-full">
+        <div className="flex flex-col items-center w-full gap-4">
             <h2 className="text-4xl font-bold">Skills</h2>
-            <div className="w-1/2 flex justify-around border-4">
-                <div className="flex-col justify-between">
+            <div className="w-1/2 flex justify-center gap-4">
+                <div className="flex flex-col justify-center">
                     <SkillIcon name='Javascript'/>
                     <SkillIcon name='html'/>
                     <SkillIcon name='css'/>
                 </div>
-                <div className="flex-col justify-between">
-                    <SkillIcon name='node'/>
-                    <SkillIcon name='express'/>
+                <div className="flex flex-col justify-center">
                     <SkillIcon name='mongodb'/>
+                    <SkillIcon name='express.js'/>
+                    <SkillIcon name='react.js'/>
+                    <SkillIcon name='node.js'/>
                 </div>
-                <div className="flex-col justify-between">
-                    <SkillIcon name='next'/>
+                <div className="flex flex-col justify-center">
+                    <SkillIcon name='next.js'/>
                     <SkillIcon name='git'/>
                     <SkillIcon name='css'/>
                 </div>
